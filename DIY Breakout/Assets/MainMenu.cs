5c71a60660d22148885f1dbc8767c7dbe4e22a10
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-20 19:06:28 +0300

    Add main menu script

---
+++ DIY Breakout/Assets/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour {
+
+    public float smoother = 1f;
+    private Quaternion menuRotation;
+
+    public AudioClip sfx;
+    AudioSource audioManager;
+
+    // Use this for initialization
+    void Start () {
+        menuRotation = transform.rotation;
+        audioManager = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            menuRotation = menuRotation * Quaternion.AngleAxis(90, Vector3.up);
+            audioManager.PlayOneShot(sfx);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            menuRotation = menuRotation * Quaternion.AngleAxis(-90, Vector3.up);
+            audioManager.PlayOneShot(sfx);
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, menuRotation, smoother * 10 * Time.deltaTime);
+    }
+}
