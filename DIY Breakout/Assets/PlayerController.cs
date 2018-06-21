26efbf45b48cc138123e3eddc1f2b041274e45ae
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-21 13:09:12 +0300

    Code added to PlayerController script

--- DIY Breakout/Assets/PlayerController.cs
+++ DIY Breakout/Assets/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour {
     public int speed = 50;
     public Transform paddle;
 
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,8 +15,20 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        // input + movement
+
         float inputHorizontal = Input.GetAxisRaw("Horizontal");
         float velocity = inputHorizontal * speed;
         paddle.transform.Translate(Vector2.right * velocity * Time.deltaTime);
+
+        // movement restriction 
+
+        if (transform.position.x <= -16.990f) {
+            transform.position = new Vector3(-16.990f, 1.014f, -19.506f);
+        }
+        if (transform.position.x >= 16.990f) {
+            transform.position = new Vector3(16.990f, 1.014f, -19.506f);
+        }
 	}
 }
