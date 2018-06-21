73d8b642ae34a67b7cf000bd93505d02131feb2a
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-21 12:57:54 +0300

    Add player controller script for the game scene

---
+++ DIY Breakout/Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public int speed = 50;
+    public Transform paddle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float inputHorizontal = Input.GetAxisRaw("Horizontal");
+        float velocity = inputHorizontal * speed;
+        paddle.transform.Translate(Vector2.right * velocity * Time.deltaTime);
+	}
+}
