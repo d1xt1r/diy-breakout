4d9280d95ed1a7a7a9d7f0b41512c5e6597f4909
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-21 14:35:41 +0300

    Add ball script

---
+++ DIY Breakout/Assets/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float speed = 10;
+    Rigidbody ballRigidbody;
+
+	// Use this for initialization
+	void Start () {
+        ballRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        ballRigidbody.AddForce(Vector3.down * speed);
+	}
+}
