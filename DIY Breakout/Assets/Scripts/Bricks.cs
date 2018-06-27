aae56a5e74c86618d59529002106437412131a4f
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-27 12:37:59 +0300

    Add OneHit, TwoHit, ThreeHit bricks functionality

--- DIY Breakout/Assets/Scripts/Bricks.cs
+++ DIY Breakout/Assets/Scripts/Bricks.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Bricks : MonoBehaviour {
 
+    public int timesHits = 0;
+    public int maxHits;
+
     public GameObject brickParticle; // variable for brick particle
 
     void OnCollisionEnter() {
+        timesHits = timesHits + 1; // every hit will increase timeshits by one
+        if (timesHits >= maxHits) {
+            GameManager.instance.DestroyBrick(); // call the DestroyBrick method from the GameManager
+            Destroy(gameObject); // destroy the brick}
+            }
         Instantiate(brickParticle, transform.position, Quaternion.identity); // instantiate brick particle effect on hit
-        GameManager.instance.DestroyBrick(); // call the DestroyBrick method from the GameManager
-        Destroy(gameObject); // destroy the brick
     }
 }
